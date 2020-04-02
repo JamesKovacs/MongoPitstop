caefdae28ed4fc205f5e6983d289cf24bc3f63a8
James Kovacs <jkovacs@post.harvard.edu>
2020-04-02 12:30:01 -0600

    Added .NET and Go Driver

--- create_version_compatibility_collection.js
+++ create_version_compatibility_collection.js
@@ -2,6 +2,16 @@ db = db.getSiblingDB('DriverVersionCompatibility');
 
 db.compatibility.drop();
 db.compatibility.insertMany([
+  { driver: "mongo-go-driver", driverVersions: [ "1.0" ], compatibleDbVersions: [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0" ] },
+  { driver: "mongo-go-driver", driverVersions: [ "1.1", "1.2", "1.3" ], compatibleDbVersions: [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+
+  { driver: "mongo-csharp-driver", driverVersions: [ "1.10", "2.0" ], compatibleDbVersions: [ "2.6", "3.0" ] },
+  { driver: "mongo-csharp-driver", driverVersions: [ "1.11", "2.2", "2.3" ], compatibleDbVersions: [ "2.6", "3.0", "3.2" ] },
+  { driver: "mongo-csharp-driver", driverVersions: [ "2.4" ], compatibleDbVersions: [ "2.6", "3.0", "3.2", "3.4" ] },
+  { driver: "mongo-csharp-driver", driverVersions: [ "2.5", "2.6" ], compatibleDbVersions: [ "2.6", "3.0", "3.2", "3.4", "3.6" ] },
+  { driver: "mongo-csharp-driver", driverVersions: [ "2.7", "2.8" ], compatibleDbVersions: [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0" ] },
+  { driver: "mongo-csharp-driver", driverVersions: [ "2.9", "2.10" ], compatibleDbVersions: [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+
   { driver: "PyMongo", driverVersions: [ "2.7" ], compatibleDbVersions: [ "2.6" ] },
   { driver: "PyMongo", driverVersions: [ "2.8", "2.9", "3.0", "3.1" ], compatibleDbVersions: [ "2.6", "3.0" ] },
   { driver: "PyMongo", driverVersions: [ "3.2", "3.3" ], compatibleDbVersions: [ "2.6", "3.0", "3.2" ] },
@@ -10,3 +20,4 @@ db.compatibility.insertMany([
   { driver: "PyMongo", driverVersions: [ "3.7", "3.8" ], compatibleDbVersions: [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0" ] },
   { driver: "PyMongo", driverVersions: [ "3.9", "3.10" ], compatibleDbVersions: [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
 ]);
+
