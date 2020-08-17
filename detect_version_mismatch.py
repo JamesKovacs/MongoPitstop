9b7b4e62388f27b686678023c1c92a61146453e3
James Kovacs <jkovacs@post.harvard.edu>
2020-08-17 10:16:25 -0600

    Updated compatibility matrix to include MongoDB 4.4 drivers

--- detect_version_mismatch.py
+++ detect_version_mismatch.py
@@ -76,12 +76,14 @@ compatibilityMatrix = [
       { "driver": "mongoc", "driverVersions": [ "1.10" ], "compatibleDbVersions": [ "3.0", "3.2", "3.4", "3.6" ] },
       { "driver": "mongoc", "driverVersions": [ "1.11", "1.12", "1.13", "1.14" ], "compatibleDbVersions": [ "3.0", "3.2", "3.4", "3.6", "4.0" ] },
       { "driver": "mongoc", "driverVersions": [ "1.15", "1.16" ], "compatibleDbVersions": [ "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+      { "driver": "mongoc", "driverVersions": [ "1.17" ], "compatibleDbVersions": [ "3.0", "3.2", "3.4", "3.6", "4.0", "4.2", "4.4" ] },
       # C++ Driver
       { "driver": "mongocxx", "driverVersions": [ "3.0" ], "compatibleDbVersions": [ "2.4", "2.6", "3.0", "3.2" ] },
       { "driver": "mongocxx", "driverVersions": [ "3.1", "3.2" ], "compatibleDbVersions": [ "2.4", "2.6", "3.0", "3.2", "3.4" ] },
       { "driver": "mongocxx", "driverVersions": [ "3.3" ], "compatibleDbVersions": [ "3.0", "3.2", "3.4", "3.6" ] },
       { "driver": "mongocxx", "driverVersions": [ "3.4" ], "compatibleDbVersions": [ "3.0", "3.2", "3.4", "3.6", "4.0" ] },
       { "driver": "mongocxx", "driverVersions": [ "3.5" ], "compatibleDbVersions": [ "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+      { "driver": "mongocxx", "driverVersions": [ "3.6" ], "compatibleDbVersions": [ "3.0", "3.2", "3.4", "3.6", "4.0", "4.2", "4.4" ] },
       # C#/.NET Driver
       { "driver": "mongo-csharp-driver", "driverVersions": [ "1.10", "2.0" ], "compatibleDbVersions": [ "2.6", "3.0" ] },
       { "driver": "mongo-csharp-driver", "driverVersions": [ "1.11", "2.2", "2.3" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2" ] },
@@ -89,9 +91,11 @@ compatibilityMatrix = [
       { "driver": "mongo-csharp-driver", "driverVersions": [ "2.5", "2.6" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6" ] },
       { "driver": "mongo-csharp-driver", "driverVersions": [ "2.7", "2.8" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0" ] },
       { "driver": "mongo-csharp-driver", "driverVersions": [ "2.9", "2.10" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+      { "driver": "mongo-csharp-driver", "driverVersions": [ "2.11" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2", "4.4" ] },
       # Go Driver
       { "driver": "mongo-go-driver", "driverVersions": [ "1.0" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0" ] },
       { "driver": "mongo-go-driver", "driverVersions": [ "1.1", "1.2", "1.3" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+      { "driver": "mongo-go-driver", "driverVersions": [ "1.4" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2", "4.4" ] },
       # Java Driver
       { "driver": "mongo-java-driver", "driverVersions": [ "2.12" ], "compatibleDbVersions": [ "2.6" ] },
       { "driver": "mongo-java-driver", "driverVersions": [ "2.13", "3.0", "3.1" ], "compatibleDbVersions": [ "2.6", "3.0" ] },
@@ -99,13 +103,15 @@ compatibilityMatrix = [
       { "driver": "mongo-java-driver", "driverVersions": [ "3.4", "3.5" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4" ] },
       { "driver": "mongo-java-driver", "driverVersions": [ "3.6", "3.7" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6" ] },
       { "driver": "mongo-java-driver", "driverVersions": [ "3.8", "3.9", "3.10" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0" ] },
-      { "driver": "mongo-java-driver", "driverVersions": [ "3.11", "3.12" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+      { "driver": "mongo-java-driver", "driverVersions": [ "3.11", "3.12", "4.0" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+      { "driver": "mongo-java-driver", "driverVersions": [ "4.1" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2", "4.4" ] },
       # Node.js "Driver"
       { "driver": "nodejs", "driverVersions": [ "1.4", "2.0" ], "compatibleDbVersions": [ "2.6", "3.0" ] },
       { "driver": "nodejs", "driverVersions": [ "2.2" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4" ] },
       { "driver": "nodejs", "driverVersions": [ "3.0" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6" ] },
       { "driver": "nodejs", "driverVersions": [ "3.1", "3.2" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0" ] },
       { "driver": "nodejs", "driverVersions": [ "3.3", "3.4", "3.5" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+      { "driver": "nodejs", "driverVersions": [ "3.6" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2", "4.4" ] },
       { "driver": "nodejs-core", "driverVersions": [ "1.4", "2.0" ], "compatibleDbVersions": [ "2.6", "3.0" ] },
       { "driver": "nodejs-core", "driverVersions": [ "2.2" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4" ] },
       { "driver": "nodejs-core", "driverVersions": [ "3.0" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6" ] },
@@ -122,7 +128,8 @@ compatibilityMatrix = [
       { "driver": "ext-mongodb:PHP", "driverVersions": [ "1.2", "1.3" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4" ] },
       { "driver": "ext-mongodb:PHP", "driverVersions": [ "1.4" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6" ] },
       { "driver": "ext-mongodb:PHP", "driverVersions": [ "1.5" ], "compatibleDbVersions": [ "3.0", "3.2", "3.4", "3.6", "4.0" ] },
-      { "driver": "ext-mongodb:PHP", "driverVersions": [ "1.6" ], "compatibleDbVersions": [ "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+      { "driver": "ext-mongodb:PHP", "driverVersions": [ "1.6", "1.7" ], "compatibleDbVersions": [ "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+      { "driver": "ext-mongodb:PHP", "driverVersions": [ "1.8" ], "compatibleDbVersions": [ "3.0", "3.2", "3.4", "3.6", "4.0", "4.2", "4.4" ] },
       # Python Driver
       { "driver": "PyMongo", "driverVersions": [ "2.7" ], "compatibleDbVersions": [ "2.6" ] },
       { "driver": "PyMongo", "driverVersions": [ "2.8", "2.9", "3.0", "3.1" ], "compatibleDbVersions": [ "2.6", "3.0" ] },
@@ -131,12 +138,15 @@ compatibilityMatrix = [
       { "driver": "PyMongo", "driverVersions": [ "3.6" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6" ] },
       { "driver": "PyMongo", "driverVersions": [ "3.7", "3.8" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0" ] },
       { "driver": "PyMongo", "driverVersions": [ "3.9", "3.10" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+      { "driver": "PyMongo", "driverVersions": [ "3.11" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2", "4.4" ] },
       # Motor Driver
       { "driver": "Motor", "driverVersions": [ "0.2", "0.3" ], "compatibleDbVersions": [ "2.6" ] },
       { "driver": "Motor", "driverVersions": [ "0.4", "0.5", "0.6" ], "compatibleDbVersions": [ "2.6", "3.0" ] },
       { "driver": "Motor", "driverVersions": [ "0.7" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2" ] },
       { "driver": "Motor", "driverVersions": [ "1.0", "1.1" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4" ] },
-      { "driver": "Motor", "driverVersions": [ "1.2" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6" ] },
+      { "driver": "Motor", "driverVersions": [ "1.2", "1.3" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6" ] },
+      { "driver": "Motor", "driverVersions": [ "2.0" ], "compatibleDbVersions": [ "3.0", "3.2", "3.4", "3.6", "4.0" ] },
+      { "driver": "Motor", "driverVersions": [ "2.1" ], "compatibleDbVersions": [ "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
       # Ruby Driver
       { "driver": "mongo-ruby-driver", "driverVersions": [ "1.8", "1.9" ], "compatibleDbVersions": [ "2.4" ] },
       { "driver": "mongo-ruby-driver", "driverVersions": [ "1.10", "1.11" ], "compatibleDbVersions": [ "2.4", "2.6" ] },
@@ -145,18 +155,20 @@ compatibilityMatrix = [
       { "driver": "mongo-ruby-driver", "driverVersions": [ "2.4" ], "compatibleDbVersions": [ "2.4", "2.6", "3.0", "3.2", "3.4" ] },
       { "driver": "mongo-ruby-driver", "driverVersions": [ "2.5" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6" ] },
       { "driver": "mongo-ruby-driver", "driverVersions": [ "2.6", "2.7", "2.8", "2.9" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0" ] },
-      { "driver": "mongo-ruby-driver", "driverVersions": [ "2.10" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+      { "driver": "mongo-ruby-driver", "driverVersions": [ "2.10", "2.11", "2.12" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+      { "driver": "mongo-ruby-driver", "driverVersions": [ "2.13" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2", "4.4" ] },
       # Rust Driver
-      { "driver": "mongo-rust-driver", "driverVersions": [ "0.9" ], "compatibleDbVersions": [ "3.6", "4.0", "4.2" ] },
+      { "driver": "mongo-rust-driver", "driverVersions": [ "0.9" ], "compatibleDbVersions": [ "3.6", "4.0", "4.2", "4.4" ] },
       # Scala Driver
       { "driver": "mongo-scala-driver", "driverVersions": [ "1.0" ], "compatibleDbVersions": [ "2.6", "3.0" ] },
       { "driver": "mongo-scala-driver", "driverVersions": [ "1.1" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2" ] },
       { "driver": "mongo-scala-driver", "driverVersions": [ "1.2", "2.0", "2.1" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4" ] },
       { "driver": "mongo-scala-driver", "driverVersions": [ "2.2", "2.3" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6" ] },
       { "driver": "mongo-scala-driver", "driverVersions": [ "2.4", "2.5", "2.6" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0" ] },
-      { "driver": "mongo-scala-driver", "driverVersions": [ "2.7", "2.8" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+      { "driver": "mongo-scala-driver", "driverVersions": [ "2.7", "2.8", "2.9" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2" ] },
+      { "driver": "mongo-scala-driver", "driverVersions": [ "4.1" ], "compatibleDbVersions": [ "2.6", "3.0", "3.2", "3.4", "3.6", "4.0", "4.2", "4.4" ] },
       # Swift Driver
-      { "driver": "MongoSwift", "driverVersions": [ "0.2" ], "compatibleDbVersions": [ "3.6", "4.0", "4.2" ] },
+      { "driver": "MongoSwift", "driverVersions": [ "1.0" ], "compatibleDbVersions": [ "3.4", "3.6", "4.0", "4.2", "4.4" ] },
     ]
 
 main()
